e1f881d8b6537fd3b352743a43e6238e99b1c585
Josh <j.larminay@gmail.com>
2017-03-04 14:47:09 -0800

    =

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ def isSnakeClose(data):
 			y = y + 1
 		x = x + 1
 		
-	print "possible -", possible	
+	#print "possible -", possible	
 	
 	for snek in snakes:
 		if(snek["id"] != uid):
@@ -70,10 +70,10 @@ def isDangerSquare(data, next):
             dangers.append([head[0], head[1] + 1])
         print "danger -", dangers
         if(snake["id"] == data["you"]):
-            if((next[0] < 0) or (next[0] > data["width"])):
+            if((next[0] < 0) or (next[0] >= data["width"])):
                 print "Wall on x plane"
                 return True
-            if((next[1] < 0) or (next[1] > data["height"])):
+            if((next[1] < 0) or (next[1] >= data["height"])):
                 print "Wall on y plane"
                 return True
 	
